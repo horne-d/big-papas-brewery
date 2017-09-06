46f5a0799e7926ade4afef16663aeb452170d2fb
Daniel Horne <d.horne92@live.co.uk>
2017-09-06 16:52:46 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// $('.accordion').on('click', '.accordion-control', function(e){ // When clicked
+//   e.preventDefault();                    // Prevent default action of button
+//   $(this)                                // Get the element the user clicked on
+//     .next('.accordion-panel')            // Select following panel 
+//     .not(':animated')                    // If it is not currently animating
+//     .slideToggle();                      // Use slide toggle to show or hide it
+// });
+
+$(document).ready(function () {
+            $('.collapse').on('shown.bs.collapse', function () {
+                $(this).parent().find('.fa-caret-right')
+                                .removeClass('fa-caret-right')
+                                .addClass('fa-caret-down');
+            }).on('hidden.bs.collapse', function () {
+                $(this).parent().find(".fa-caret-down")
+                                .removeClass("fa-caret-right")
+                                .addClass("fa-caret-right");
+            });
+        });
