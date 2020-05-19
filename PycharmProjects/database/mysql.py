37d2e3d6c21b33a39375556d5fb0ff7dfdb13b06
Dan Horne <d.horne92@live.co.uk>
2020-05-19 16:34:48 +0100

    Updates

--- PycharmProjects/database/mysql.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb as _mysql
-
-db = _mysql.connect(
-    db='nutrition',
-    host='localhost',
-    user='horned',
-    passwd='password')
-
---
+++ precache-manifest.b56ad8963f207f1792846ed2aaa6a58d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b505dcd69ec8c48728fc0cc9da0ecf",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "d82f9ac9ca01c7be2ee2",
+    "url": "/weatherapp/static/css/main.b9241913.chunk.css"
+  },
+  {
+    "revision": "5614467a4f5e60e40083",
+    "url": "/weatherapp/static/js/2.b45a3554.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.b45a3554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82f9ac9ca01c7be2ee2",
+    "url": "/weatherapp/static/js/main.a2bc2933.chunk.js"
+  },
+  {
+    "revision": "26373162a9e6f2118022",
+    "url": "/weatherapp/static/js/runtime-main.d5bba109.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weatherapp/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weatherapp/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
